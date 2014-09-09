66bee7bfcfe934850d8b417ddb3f8dd58d0674d3
Julia Morneau <Julia@julias-mbp.wheatonma.edu>
2014-09-09 13:29:37 -0400

    attempting hw04 : many questions

--- hw04/Markdown.py
+++ hw04/Markdown.py
@@ -1,7 +1,29 @@
 # Julia Morneau
 
 def get_file(self, filename):
+	""" Gets the markdown file and splits it on every line
+
+	args:
+		filename: a markdown file
+	returns:
+		returns a list of every line in the file
+	"""
+
 	file = open(filename, "r")
 	string = file.split("\n")
 
 	file.close()	
+	return string
+
+def markdown(self, list):
+	dict = {'#':<h1>,
+		'##':<h2>,
+		'###':<h3>,
+		'####':<h4>,
+		'#####':<h5>,
+		'######:<h6>,
+		'* ':<li>,
+		'**':<em>,
+		'[':<a>,
+		'(':<href>
+		}
