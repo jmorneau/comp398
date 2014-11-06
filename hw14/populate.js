d87d8d3705838150b62a3e7e7353d3686c21d79b
Julia Morneau <morneau_julia@wheatoncollege.edu>
2014-11-06 13:52:10 -0500

    finished hw14

--- hw14/populate.js
+++ hw14/populate.js
@@ -17,20 +17,13 @@ MongoClient.connect("mongodb://jmorneau:julia1117@ds051640.mongolab.com:51640/co
 
   	var doc1 = {'hello':'doc1'};
   	var doc2 = {'hello':'doc2'};
-  	var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+  	var lotsOfDocs= []; 
 
   	for (i = 0; i < 2500; i++)
 	{
-		if(i%3==0){
-			collection.insert(doc1, function(err, result) {});
-		}
-		if(i%3==1){
-			collection.insert(doc2, function(err, result) {});
-		}
-		if(i%3==2){
-			collection.insert(lotsOfDocs, function(err, result) {});
-		}
+			lotsOfDocs.push({i:i});
 	}
+	collection.insert(lotsOfDocs, function(err, result) {});
 	console.dir('added to hw14');
 
 });
